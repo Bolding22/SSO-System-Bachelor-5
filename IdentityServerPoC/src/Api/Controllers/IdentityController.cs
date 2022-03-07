241e6f0ebbb2797784cb4193d8b16bf2b3256c35
Bolding22 <w.bolding@outlook.com>
2022-03-07 08:40:57 +0100

    Upload project

---
+++ IdentityServerPoC/src/Api/Controllers/IdentityController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+[Route("identity")]
+[Authorize]
+public class IdentityController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return new JsonResult(User.Claims.Select(c => new {c.Type, c.Value}));
+    }
+}
