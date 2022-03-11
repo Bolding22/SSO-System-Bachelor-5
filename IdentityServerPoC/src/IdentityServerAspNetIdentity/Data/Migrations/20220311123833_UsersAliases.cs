157e0d2d6abb4ec6858031c8d5d4fd6177331018
AndreasEdal <anepe19@student.sdu.dk>
2022-03-11 13:43:00 +0100

    The actual migration

---
+++ IdentityServerPoC/src/IdentityServerAspNetIdentity/Data/Migrations/20220311123833_UsersAliases.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IdentityServerAspNetIdentity.Data.Migrations
+{
+    public partial class UsersAliases : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserAliases",
+                columns: table => new
+                {
+                    SystemUserId = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    OrganizationId = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    ApplicationUserId = table.Column<string>(type: "varchar(255)", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserAliases", x => x.SystemUserId);
+                    table.ForeignKey(
+                        name: "FK_UserAliases_AspNetUsers_ApplicationUserId",
+                        column: x => x.ApplicationUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserAliases_ApplicationUserId",
+                table: "UserAliases",
+                column: "ApplicationUserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserAliases");
+        }
+    }
+}
