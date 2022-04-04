86069f73b3d4640ce111e186542678049887859a
AndreasEdal <anepe19@student.sdu.dk>
2022-04-04 14:01:14 +0200

    Batch files building and pushing docker images to Dockerhub

---
+++ IdentityServerPoC/push-idp-to-dockerhub.bat
@@ -0,0 +1,2 @@
+docker build -f ./src/IdentityServerAspNetIdentity/Dockerfile -t andreasedal/ajour-sso-idp .
+docker push andreasedal/ajour-sso-idp
---
+++ IdentityServerPoC/push-sp-to-dockerhub.bat
@@ -0,0 +1,2 @@
+docker build -f .\src\AjourBuildMock\Dockerfile -t andreasedal/ajour-sso-sp .
+docker push andreasedal/ajour-sso-sp
