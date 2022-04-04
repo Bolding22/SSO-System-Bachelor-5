1d72004fb3f9094767de02129da0475a57ed9b90
AndreasEdal <anepe19@student.sdu.dk>
2022-04-04 09:03:39 +0200

    Clean up of ProfileService

--- IdentityServerPoC/src/IdentityServerAspNetIdentity/ClaimHandling/ProfileService.cs
+++ IdentityServerPoC/src/IdentityServerAspNetIdentity/ClaimHandling/ProfileService.cs
@@ -1,36 +1,27 @@
 ﻿using System.Security.Claims;
 using Duende.IdentityServer.Models;
 using Duende.IdentityServer.Services;
-using IdentityServerAspNetIdentity.Models;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace IdentityServerAspNetIdentity.ClaimHandling;
 
 public class ProfileService : IProfileService
 {
-    private readonly UserManager<ApplicationUser> _userManager;
-
-    public ProfileService(UserManager<ApplicationUser> userManager)
-    {
-        _userManager = userManager;
-    }
-    
     /// <inheritdoc/>
-    public async Task GetProfileDataAsync(ProfileDataRequestContext context)
+    public Task GetProfileDataAsync(ProfileDataRequestContext context)
     {
-        var enumerable = context.RequestedResources.Resources.IdentityResources.SelectMany(resource => resource.UserClaims);
-        context.RequestedClaimTypes = enumerable;
-        var requestedClaimTypes = context.RequestedClaimTypes;
+        var identityResources = context.RequestedResources.Resources.IdentityResources;
+        var userClaims = identityResources.SelectMany(resource => resource.UserClaims);
+        
+        context.RequestedClaimTypes = userClaims;
 
-        if (requestedClaimTypes.Any())
+        if (context.RequestedClaimTypes.Any())
         {
-            var user = await _userManager.GetUserAsync(context.Subject);
-            var claims = await _userManager.GetClaimsAsync(user);
             context.AddRequestedClaims(context.Subject.Claims);
         }
         
         context.IssuedClaims.Add(new Claim("test-claim", "test-value"));
+        return Task.CompletedTask;
     }
 
     /// <inheritdoc/>
